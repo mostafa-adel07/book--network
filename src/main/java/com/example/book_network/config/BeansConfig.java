55aea358fa8db6e12c1d085618f8678c501a953a
MohamedAdeLMohammed <mostafa-adel07>
2025-01-26 16:44:43 +0200

    Create the Book,Feedback and BookTransactionHistory entities Add the relationships between the entities Add the application auditor aware:: Auditing

--- src/main/java/com/example/book_network/config/BeansConfig.java
+++ src/main/java/com/example/book_network/config/BeansConfig.java
@@ -3,11 +3,11 @@ package com.example.book_network.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,6 +29,11 @@ public class BeansConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
     }
 
     @Bean
--- src/main/java/com/example/book_network/user/User.java
+++ src/main/java/com/example/book_network/user/User.java
@@ -1,5 +1,7 @@
 package com.example.book_network.user;
 
+import com.example.book_network.book.Book;
+import com.example.book_network.history.BookTransactionHistory;
 import com.example.book_network.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
@@ -43,6 +45,11 @@ public class User implements UserDetails, Principal {
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
 
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    @OneToMany(mappedBy = "user")
+    private List<BookTransactionHistory> histories;
 
 
     @CreatedDate
