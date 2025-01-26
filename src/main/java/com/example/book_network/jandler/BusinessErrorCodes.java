6f6ad100710e30b7f9b1c3f09ad83b9c9212fe7a
MohamedAdeLMohammed <mostafa-adel07>
2025-01-26 15:34:44 +0200

    Implement the exception handler

---
+++ src/main/java/com/example/book_network/jandler/BusinessErrorCodes.java
@@ -0,0 +1,28 @@
+package com.example.book_network.jandler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+@Getter
+
+public enum BusinessErrorCodes {
+    NO_CODE(0,HttpStatus.NOT_IMPLEMENTED, "No code"),
+    INCORRECT_CURRENT_PASSWORD(300, HttpStatus.BAD_REQUEST, "Current password is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, HttpStatus.BAD_REQUEST, "New password does not match"),
+    ACCOUNT_DISABLED(303, HttpStatus.FORBIDDEN, "Account is disabled"),
+    ACCOUNT_LOCKED(302, HttpStatus.FORBIDDEN, "User account is locked"),
+    BAD_CREDENTIALS(304, HttpStatus.UNAUTHORIZED, "Login email or password is incorrect"),
+
+    ;
+
+
+    private final int code;
+    private final String description;
+    private final HttpStatus httpStatus;
+
+
+    BusinessErrorCodes(int code, HttpStatus httpStatus ,String description) {
+        this.code = code;
+        this.description = description;
+        this.httpStatus = httpStatus;
+    }
+}
